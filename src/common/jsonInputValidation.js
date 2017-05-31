e07900937e1cbd00fec89af2458462bd552d6466
Gaurav Singh. Kushwaha <gkushwah@oradev.oraclecorp.com>
2017-05-31 11:05:49 +0530

    minor

--- src/common/jsonInputValidation.js
+++ src/common/jsonInputValidation.js
@@ -4,12 +4,17 @@ var Validator = require('jsonschema').Validator;
 var v = new Validator();
 
 module.exports = function (jsonData) {
-    const schema = conf.resourceSchema.get(jsonData.table);
-    log.debug('jsonData = ' + JSON.stringify(jsonData) + '\n schema = ' + JSON.stringify(schema));
-    var result = v.validate(jsonData, schema);
-    if (result.errors != null && result.errors.length > 0) {
-        log.error("JSON validation failed for errors: " + result.errors.join(", "));
-        throw new Error('Json validation failed. JSON SCHEMA = '+JSON.stringify(schema));
+    try {
+        const schema = conf.resourceSchema.get(jsonData.table);
+        log.debug('jsonData = ' + JSON.stringify(jsonData) + '\n schema = ' + JSON.stringify(schema));
+        var result = v.validate(jsonData, schema);
+        if (result.errors != null && result.errors.length > 0) {
+            log.error("JSON validation failed for errors: " + result.errors.join(", "));
+            throw new Error('Json validation failed. JSON SCHEMA = ' + JSON.stringify(schema));
+        }
+    } catch (err) {
+        log.error("JSON validation failed for errors: " + err);
+        throw new Error('Json validation failed');
     }
     return true;
 }
