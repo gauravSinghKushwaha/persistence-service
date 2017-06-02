825419426f97ffb2a7ba7d4803a041885473967d
gauravSinghKushwaha <gkushwah@oradev.oraclecorp.com>
2017-06-02 12:04:14 +0530

    insert done

--- src/web/routes/authentication.js
+++ src/web/routes/authentication.js
@@ -8,6 +8,7 @@ const express = require('express');
 const auth = require('basic-auth');
 const router = express.Router();
 const config = conf.config;
+
 /**
  * Middleware woudl be used for authentication
  * @param  {[type]}   req  [description]
@@ -43,23 +44,17 @@ router.route('/resources').post(function (req, res) {
         }
         queryBuilder = new QueryBuilder(req, res, jsonValidator.getSchema(req.body));
         query = queryBuilder.createInsertQuery();
-        try {
-            connection.query(query.query, query.values, function (error, results, fields) {
-                connection.release();
-                if (error) {
-                    log.error(error);
-                    res.status(400);
-                    return res.send(error.toString());
-                }
-                log.debug('results = ' + results + '\nfields = ' + fields);
-                res.status(201);
-                return res.send(JSON.stringify(results.insertId));
-            });
-        } catch (err) {
-            log.error('error executing query = ' + err);
-            res.status(500);
-            return res.send('error getting connection =' + err.toString());
-        }
+        connection.query(query.query, query.values, function (error, results, fields) {
+            connection.release();
+            if (error) {
+                log.error(error);
+                res.status(400);
+                return res.send(error.toString());
+            }
+            log.debug('results = ' + results + '\nfields = ' + fields);
+            res.status(201);
+            return res.send(JSON.stringify(results.insertId));
+        });
     });
 });
 
@@ -67,48 +62,4 @@ router.route('/password').get(function (req, res) {
     res.status(200).send('{password:password}');
 });
 
-/*
- apiRoutes.post('/authenticate', function(req, res) {
-
- // find the user
- User.findOne({
- name: req.body.name
- }, function(err, user) {
-
- if (err) throw err;
-
- if (!user) {
- res.json({
- success: false,
- message: 'Authentication failed. User not found.'
- });
- } else if (user) {
-
- // check if password matches
- if (user.password != req.body.password) {
- res.json({
- success: false,
- message: 'Authentication failed. Wrong password.'
- });
- } else {
-
- // if user is found and password is right
- // create a token
- var token = jwt.sign(user, app.get('superSecret'), {
- expiresInMinutes: 1440 // expires in 24 hours
- });
-
- // return the information including token as JSON
- res.json({
- success: true,
- message: 'Enjoy your token!',
- token: token
- });
- }
-
- }
-
- });
- });
- */
 module.exports = router;
